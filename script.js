0dfc9b4af48d7dee3426463ddea7a8c913a3bc3b
Christopher <127637098+Krisstopher15@users.noreply.github.com>
2023-04-23 18:08:38 -0400

    Added: Random colors, reset buttons.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const gridContainer = document.querySelector(".grid-container");
-const resetButton = document.querySelector(".btn-reset");
+const sizeButton = document.querySelector(".btn-size");
+const clearButton = document.querySelector(".btn-clear");
+const randomButton = document.querySelector(".btn-random");
+const blackButton = document.querySelector(".btn-black");
 
 const squares = (n) => {
   gridContainer.style.gridTemplateColumns = `repeat(${n} , 1fr)`;
@@ -15,4 +18,49 @@ const squares = (n) => {
   }
 };
 
-squares(64);
+const getSize = () => {
+  gridContainer.innerHTML = "";
+  let size = prompt("Size of Sketch");
+
+  if (size > 2 && size <= 100) {
+    gridContainer.innerHTML = "";
+    squares(size);
+  } else {
+    alert("Error");
+    gridContainer.innerHTML = "";
+  }
+};
+
+const reset = () => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.style.backgroundColor = "#dbd7d2";
+  });
+};
+
+const randomColors = () => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      square.style.backgroundColor = "#" + randomColor;
+    });
+  });
+};
+
+const blackColor = () => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", () => {
+      square.style.backgroundColor = "#000";
+    });
+  });
+};
+
+blackButton.addEventListener("click", () => blackColor());
+randomButton.addEventListener("click", () => randomColors());
+clearButton.addEventListener("click", () => reset());
+sizeButton.addEventListener("click", () => getSize());
